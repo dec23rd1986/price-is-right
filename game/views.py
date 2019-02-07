20af3482841ed03afe7666d0c0cbf0486d8d2382
Andrea <33186021+dec23rd1986@users.noreply.github.com>
2019-02-06 22:20:29 -0500

    updated views.py

--- game/views.py
+++ game/views.py
@@ -3,10 +3,6 @@ from django.contrib.auth.forms import UserCreationForm
 from accounts.views import signup
 from .models import Game
 
-# def game_start(request):
-#     games = Game.objects.all().order_by('date') # grabs all records in game in db table, order by date
-#     return render (request, 'game/game_start.html', {'games':games})
-
 def game_start(request):
     games = Game.objects.all().order_by('date')
     data = {'msg': ''}
--- priceisright/views.py
+++ priceisright/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 
 def about(request):
     # return HttpResponse('Price is right game one')
+    # this will be a simple about me page / will list website info
     return render(request, 'about.html')
