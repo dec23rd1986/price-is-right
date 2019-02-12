d031dcaab463bae0732fa83a7314e63100df717e
Andrea <33186021+dec23rd1986@users.noreply.github.com>
2019-02-11 22:28:30 -0500

    updated views.py

--- game/views.py
+++ game/views.py
@@ -6,14 +6,11 @@ from .models import Game
 
 def game_start(request):
     games = Game.objects.all().order_by('date')
-    data = {'msg': ''}
     response = signup(request)
     if response['user_created']: #if user is created we are directing them to game 1
         return render (request, 'retailPrice/retailPrice_start.html')
-        data['msg'] = 'Thanks for being the part of our beautiful community!'
     else: #else we are redirecting them back to homepage for now
         return render(request, 'game/game_start.html', {
                 'games':games,
                 'form': response['form'],
-                'msg': data['msg']
     })
