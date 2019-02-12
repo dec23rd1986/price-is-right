17c4c9ad978c58f8f06e8390654aa170942b806d
Andrea <33186021+dec23rd1986@users.noreply.github.com>
2019-02-11 22:22:50 -0500

    added redirect req after user signup

--- game/views.py
+++ game/views.py
@@ -8,11 +8,11 @@ def game_start(request):
     games = Game.objects.all().order_by('date')
     data = {'msg': ''}
     response = signup(request)
-    if response['user_created']:
-        
-       # you can redirect user here to somewhere else when they have been registered.
+    if response['user_created']: #if user is created we are directing them to game 1
+        return render (request, 'retailPrice/retailPrice_start.html')
         data['msg'] = 'Thanks for being the part of our beautiful community!'
-    return render(request, 'game/game_start.html', {
+    else: #else we are redirecting them back to homepage for now
+        return render(request, 'game/game_start.html', {
                 'games':games,
                 'form': response['form'],
                 'msg': data['msg']
