7ef34e12fff6cebf62c7c5c94e41e4fe98c693f5
Andrea <33186021+dec23rd1986@users.noreply.github.com>
2019-02-17 00:38:01 -0500

    initial commit

--- retailPrice/templatetags/my_templatetag.py
+++ retailPrice/templatetags/my_templatetag.py
@@ -1,4 +1,4 @@
-import random
+import random, os
 from django import template
 
 
@@ -9,3 +9,17 @@ def random_int(a, b=None):
     if b is None:
         a, b = 0, a
     return random.randint(a, b)
+
+#Generate random img function
+def random_filename(width=128, height=128):
+    path = r"C:\Users\Andrea\Desktop\Game\priceisright\assets\img"
+    random_filename = random.choice([
+    x for x in os.listdir(path)
+    if os.path.isfile(os.path.join(path, x))
+])
+    return random_filename()
+
+
+# grill = 139.99, Kenmore 4-Burner Gas Grill with Side Burner
+# toothpaste = 2.99, Crest Cavity Protection
+#coffee maker = 2,730, automatic espresso coffee machine
